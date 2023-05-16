fe57f7365aad36eb29e3f69ccc2c9c2f7b716d9f
estephany <estephany.perez@bbva.com>
2023-05-16 09:04:06 -0500

    PERAR-22931 feat: using listCustomers service to get birthdate for black list payload - easy yes product

--- artifact/libraries/PISDR008/src/main/java/com/bbva/pisd/lib/r008/PISDR008.java
+++ artifact/libraries/PISDR008/src/main/java/com/bbva/pisd/lib/r008/PISDR008.java
@@ -3,11 +3,11 @@ package com.bbva.pisd.lib.r008;
 import com.bbva.pisd.dto.insurance.bo.BlackListIndicatorBO;
 import com.bbva.pisd.dto.insurance.bo.SelectionQuotationPayloadBO;
 import com.bbva.pisd.dto.insurance.commons.IdentityDataDTO;
-
+import com.bbva.pisd.dto.insurance.aso.CustomerListASO;
 public interface PISDR008 {
 
 	BlackListIndicatorBO executeGetBlackListIndicatorService(String customerId);
 	SelectionQuotationPayloadBO executeGetBlackListRiskService(IdentityDataDTO input, String traceId);
 	SelectionQuotationPayloadBO executeGetBlackListHealthService(IdentityDataDTO input, String traceId);
-
+	CustomerListASO executeGetCustomerInformation(String customerId);
 }
