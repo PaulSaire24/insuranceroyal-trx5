bddb6584f7030a2bf970d5262761d09cc8666b68
jhon.aguilar <jhon.aguilar@bbva.com>
2023-05-30 16:53:06 -0500

    PERAR-22926 fix: removing unused imports

--- artifact/libraries/PISDR018IMPL/src/main/java/com/bbva/pisd/lib/r018/impl/PISDR018Impl.java
+++ artifact/libraries/PISDR018IMPL/src/main/java/com/bbva/pisd/lib/r018/impl/PISDR018Impl.java
@@ -16,9 +16,6 @@ import com.bbva.pisd.dto.insurance.utils.PISDErrors;
 import com.bbva.pisd.dto.insurance.utils.PISDValidation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
